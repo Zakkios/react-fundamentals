32ace2016d661d49054a2259a16487bd57b57f75
Zakkios <laskari.zakaria@gmail.com>
2023-08-11 09:52:31 +0200

    exercise 5 + extra 1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -22,6 +22,22 @@ const Box = (className, style, children) => {
   )
 }
 
+const smallBox = (
+  <div className="box box--small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </div>
+)
+const mediumBox = (
+  <div className="box box--medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </div>
+)
+const largeBox = (
+  <div className="box box--large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </div>
+)
+
 function App() {
   return (
     <div>
@@ -29,10 +45,10 @@ function App() {
         small lightblue box
       </Box>
       <Box className="box--medium" style={{backgroundColor: 'pink'}}>
-        small lightblue box
+        medium pink box
       </Box>
       <Box className="box--large" style={{backgroundColor: 'orange'}}>
-        small lightblue box
+        large orange box
       </Box>
     </div>
   )
